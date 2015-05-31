27e1b7fbc7a41511f38a152e90e62e25fd1cc96a
garanaveen <garanaveen@gmail.com>
2015-05-31 01:41:53 -0500

    possible words for the given letters and size.
    Added helper file which finds all the possible words for the given letters and size.

---
+++ 4pics1word_helper.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import argparse
+
+
+def DeleteALetterFromString(character, s):
+
+    l = list(s)  # convert to list
+    
+    p = l.index(character)  # find position of the letter "a"
+    del(l[p])         # delete it
+    
+    s = "".join(l)  # convert back to string
+    return s
+
+def DoesWordMeetsAllLetters(word):
+    returnValue = True
+    allowedLetters = allowedlettersInCAPS
+    for i in word:
+        if(i.upper() not in allowedLetters):
+            #print "Returning False for word", word, "as", i , "not present in ", allowedLetters
+            returnValue = False
+            break
+        else:
+            #print "Deleting", i, "from", allowedLetters
+            #Delete that character from the allowedString
+            allowedLetters = DeleteALetterFromString(i.upper(), allowedLetters)
+            #print "After Deleting", i, "NewAllowedLetters is ", allowedLetters
+
+    #print "Returning", returnValue, "for word", word
+    return returnValue
+
+def DoesWordMeetsCondition(word):
+    if(len(word) == int(args.sizeofword) and DoesWordMeetsAllLetters(word)):
+        return True
+    else:
+        return False
+
+def PrintLinesInFile():
+    f = open('british-english')
+    try:
+        for line in f:
+            words = line.split()
+            if (words):
+                if (True == DoesWordMeetsCondition(words[0])):
+                    print words[0]
+    finally:
+        f.close()
+
+
+
+
+parser = argparse.ArgumentParser(description='4in1 game parameter parser')
+parser.add_argument('--letters', dest='allowedletters', required=True, help='All possible english alphabets (enter without any space)')
+parser.add_argument('--size', dest='sizeofword', required=True, help='Size of the word')
+
+args = parser.parse_args()
+print "allowedLetters:", args.allowedletters
+print "sizeOfWord:", args.sizeofword
+allowedlettersInCAPS = args.allowedletters
+allowedlettersInCAPS = allowedlettersInCAPS.upper()
+print "allowedlettersInCAPS:", allowedlettersInCAPS
+
+PrintLinesInFile()
+
+
+
+#def ParseEnglishDictionaryFile(letters, sizeofword)
+#Code to parse
+#return all possible words which meet the condition of letters and sizeofword.
+
+
+#def IsValid()
+#Given a word, this function decides if this word meets the condition of letters and sizeofword.
+
+
